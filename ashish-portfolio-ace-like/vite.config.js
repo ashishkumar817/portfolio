29760b8775189992a1d33ef8ec3126e77be71118
Ashish Kumar <ashishkumarkaup@gmail.com>
2025-08-17 17:22:54 +0530

    Update vite.config.js

--- ashish-portfolio-ace-like/vite.config.js
+++ ashish-portfolio-ace-like/vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/ashish-portfolio-ace-like/', // must match your repo name exactly!
+  base: '/portfolio/', // must match your repo name exactly!
 })
